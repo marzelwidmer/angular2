11fd0837b70832133c890b3fa798b0742c7b81a7
Marcel Widmer <marzelwidmer@gmail.com>
2016-12-02 08:03:37 +0100

    add some comment in typescript file app.component.ts

--- app/app.components.ts
+++ app/app.components.ts
@@ -3,14 +3,20 @@
  */
 import {Component} from "@angular/core";
 
+// Decorator Component
+// Marks a class as an Angular component and collects component configuration metadata.
 @Component({
+    // The CSS selector that triggers the instantiation of a directive.
     selector: 'app',
+    // Specifies an inline template for an Angular component.
+    // Only one of `templateUrl` or `template` can be defined per Component.
     template: `
                 <nav>Navigation</nav>
                 <main>Main</main>
                 <footer>Fotter</footer>
     `
 })
+// Class
 export class AppComponent {
 
     constructor() {
