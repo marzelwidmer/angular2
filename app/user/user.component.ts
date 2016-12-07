176a5b90ae1e7e86a8f184c43b3cdc7e120b77ee
Marcel Widmer <marzelwidmer@gmail.com>
2016-12-07 11:39:47 +0100

    interpolation

--- app/user/user.component.ts
+++ app/user/user.component.ts
@@ -5,18 +5,28 @@ import {Component} from "@angular/core";
   template: `
     <section>
         <header>
-            Marcel Widmer
+         {{username}}   
         </header>
         <p>
-        Visitors, powers, and crystal winds will always protect them.
-        Nunquam imitari contencio.
-        Pol.Ubi est domesticus frondator?
-        Desolation is a warm dagger.
+        {{getDect()}}
         </p>
+        <p>
+            {{getSum(1,9)}}
+        </p>      
     </section>
     `
 })
 
 export class UserComponent {
+  username: String = "Marcel Widmer";
+
+  getSum(num1: number, num2: number): number {
+    return num1 + num2;
+  }
+
+  getDect(): string {
+    return 'Visitors, powers, and crystal winds will always protect them. ' +
+      'Nunquam imitari contencio.Pol.Ubi est domesticus frondator Desolation is a warm dagger.';
+  }
 
 }
