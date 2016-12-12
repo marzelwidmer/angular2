847326f2d9579bc2e25ea7f80a3ae1a8a1633af0
Marcel Widmer <marzelwidmer@gmail.com>
2016-12-12 14:06:41 +0100

    own directive events

--- app/utils/italic.directive.ts
+++ app/utils/italic.directive.ts
@@ -1,10 +1,21 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
     selector: '[myItalic]'
 })
 export class MyItalic {
 
-    @HostBinding('class.italic') isItalic: boolean = true;
+    @HostBinding('class.italic') isItalic: boolean = false;
+
+    @HostListener('mouseenter')
+    onMouseEnter(){
+        this.isItalic = true;
+    }
+
+    @HostListener('mouseleave')
+    onMouseLeave(){
+        this.isItalic = false;
+    }
+
 
 }
